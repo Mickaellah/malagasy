5bb5586b9a475a7385a7724b3422b4e1c9f0ea61
Clopedia <clopedia.nom@onja.org>
2021-04-24 15:48:26 +0300

    Added some styles to the buttons in the ActionButton folder's file, tried several styles to get it right but didn't work yet.

--- src/components/ActionButton/ActionButton.js
+++ src/components/ActionButton/ActionButton.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {
+    View, 
+    Text, 
+    TouchableOpacity, 
+    Image, 
+    StyleSheet
+} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
-        justifyContent: 'center',
         margin: 0,
         padding: 0,
     },
+    button: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        fontSize: 16,
+        lineHeight: 19,
+        fontWeight: '600',
+        paddingTop: 10,
+    },
     text: {
-        textAlign: 'justify',
         alignSelf: 'flex-end',
+        textAlign: 'right',
+
     },
     image: {
         marginLeft: 11,
@@ -18,12 +32,24 @@ const styles = StyleSheet.create({
     }
 })
 
-export default function ActionButton({buttonText, onPress, icon, textColor}) {
+export default function ActionButton({
+    buttonText, 
+    onPress, 
+    icon, 
+    textColor
+}) {
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={onPress} style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text style={[textColor, styles.text]}>{buttonText}</Text>
-                <Image source={icon} style={styles.image} />
+            <TouchableOpacity 
+                onPress={onPress} 
+                style={styles.button}
+            >
+                <Text style={[textColor, styles.text]}> {buttonText}
+                </Text>
+                <Image 
+                    source={icon} 
+                    style={styles.image} 
+                />
             </TouchableOpacity>
         </View>
     )
--- src/components/ActionButton/ActionButton.stories.js
+++ src/components/ActionButton/ActionButton.stories.js
@@ -22,14 +22,34 @@ const styles = StyleSheet.create({
 })
 
 storiesOf('Action button', module)
-    .addDecorator(story => <View>{story()}</View>)
+    .addDecorator(story => <View style={{padding: 20}
+    }>{story()}</View>)
     .add('Action Button', () => {
         return (
             <View>
-                <ActionButton buttonText="Learn" onPress={() => alert('Learn more!')} icon={RightArrow} textColor={styles.blueText} />
-                <ActionButton buttonText="Pick" onPress={() => alert('Pick a solution.')} icon={RightArrow} textColor={styles.blueText} />
-                <ActionButton buttonText="Correct" onPress={() => alert('Your answer is correct.')} icon={CorrectAnswer} textColor={styles.greenText} />
-                <ActionButton buttonText="Wrong" onPress={() => alert('Your answer is wrong.')} icon={WrongAnswer} textColor={styles.purpleText} />
+                <ActionButton 
+                    buttonText="Learn" 
+                    onPress={() => alert('Learn more!')} icon={RightArrow} 
+                    textColor={styles.blueText} 
+                />
+                <ActionButton 
+                    buttonText="Pick" 
+                    onPress={() => alert('Pick a solution.')} 
+                    icon={RightArrow} 
+                    textColor={styles.blueText} 
+                />
+                <ActionButton 
+                    buttonText="Correct" 
+                    onPress={() => alert('Your answer is correct.')} 
+                    icon={CorrectAnswer} 
+                    textColor={styles.greenText} 
+                />
+                <ActionButton 
+                    buttonText="Wrong" 
+                    onPress={() => alert('Your answer is wrong.')} 
+                    icon={WrongAnswer} 
+                    textColor={styles.purpleText} 
+                />
             </View>
         )
     })
