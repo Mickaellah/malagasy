85547abdaf8af836080bf2eb05324abebf2aebcf
Clopedia <clopedia.nom@onja.org>
2021-04-27 10:59:37 +0300

    Added two states in the LanguageSwitcherButton.stories.js file for handling the two languages.

--- src/components/LanguageSwitcherButton/LanguageSwitcherButton.js
+++ src/components/LanguageSwitcherButton/LanguageSwitcherButton.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, Stylesheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-export default function LanguageSwitcherButton() {
+const styles = StyleSheet.create({
+  container: {
+    margin: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 13,
+    backgroundColor: '#06B6D4',
+    textAlignVertical: 'center',
+    borderRadius: 30,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    textTransform: 'uppercase',
+    fontSize: 13,
+    lineHeight: 16,
+    fontWeight: '400',
+  },
+  leftText: {
+    marginRight: 6,
+  },
+  rightText: {
+    marginLeft: 6,
+  },
+});
+
+export default function LanguageSwitcherButton({
+  english,
+  malagasy,
+  onPress,
+  icon,
+}) {
   return (
     <View>
-      <TouchableOpacity>
-        <Text>En</Text>
-        <Text>Ma</Text>
+      <TouchableOpacity onPress={onPress} style={styles.button}>
+        <View style={styles.wrapper}>
+          <Text style={[styles.buttonText, styles.leftText]}>{english}</Text>
+          {icon}
+          <Text style={[styles.buttonText, styles.rightText]}>{malagasy}</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
