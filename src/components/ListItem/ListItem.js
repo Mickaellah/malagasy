306eead952fb18e39a69cbe07861f3fc5b0583a2
Clopedia <clopedia.nom@onja.org>
2021-04-28 15:13:40 +0300

    Imported the Action button to the ListItem.js file in the ListItem component and passed all of the apropriate props for it, added a wraper for the text and the ActionButton component and added styles to it.

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import {View, Text, TouchableWithoutFeedback, StyleSheet} from 'react-native';
 
+import ActionButton from '../ActionButton/ActionButton';
+import Arrow from '../../icons/arrow.svg';
+
 const styles = StyleSheet.create({
-  list: {
+  container: {
     paddingHorizontal: 16,
     paddingVertical: 17,
+    margin: 0,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 0,
+    margin: 0,
   },
   text: {
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 19,
+    textAlignVertical: 'center',
+  },
+  learnButtonColor: {
+    color: '#06B6D4',
     fontSize: 16,
     fontWeight: '400',
     lineHeight: 19,
@@ -15,9 +32,17 @@ const styles = StyleSheet.create({
 
 export default function ListItem({name, onPress}) {
   return (
-    <View>
-      <TouchableWithoutFeedback style={styles.list} onPress={onPress}>
-        <Text style={styles.text}>{name}</Text>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={onPress}>
+        <View style={styles.wrapper}>
+          <Text style={styles.text}>{name}</Text>
+          <ActionButton
+            buttonText={'Learn'}
+            icon={<Arrow />}
+            textColor={styles.learnButtonColor}
+            onPress={() => alert('Learn more!')}
+          />
+        </View>
       </TouchableWithoutFeedback>
     </View>
   );
